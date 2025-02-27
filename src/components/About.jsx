b8e7d4a1dd0e0eaf21047419b3295fb09f59e5ea
USER <vusumuzi1947@gmail.com>
2025-02-27 13:55:31 +0200

    completed about component + started with projects component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { assets } from "../assets/assets";
 const About = () => {
   return (
     <div
@@ -7,10 +7,50 @@ const About = () => {
       id="About"
     >
       <h1 className="text-2xl sm:text-4xl font-bold mb-2">
-        About
-        <span className="underline underline-offset-4 decoration-1 under font-light">Our Brand</span>
+        About 
+         <span className="underline underline-offset-4 decoration-1 under font-light">
+           Our Brand
+        </span>
       </h1>
-      <p className="text-gray-500 max-w-80 text-center mb-8">Passionate About Properties, Dedicated to Your Vision</p>
+      <p className="text-gray-500 max-w-80 text-center mb-8">
+        Passionate About Properties, Dedicated to Your Vision
+      </p>
+      <div className="flex flex-col md:flex-row items-center md:items-start md:gap-20">
+        <img
+          src={assets.brand_img}
+          alt="brand-image"
+          className="w-full sm:w-1/2 max-w-lg"
+        />
+        <div className="flex flex-col items-center md:items-start mt-10 text-gray-600">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
+            <div>
+              <p className="">10+</p>
+              <p className="">Years of Excellence</p>
+            </div>
+            <div>
+              <p className="">12+</p>
+              <p className="">Projects Completed</p>
+            </div>
+            <div>
+              <p className="">20+</p>
+              <p className="">Mn. Sq. Ft Delivered</p>
+            </div>
+            <div>
+              <p className="">34+</p>
+              <p className="">Ongoing Projects</p>
+            </div>
+          </div>
+          <p className="my-10 max-w-lg">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint
+            veniam illum nihil blanditiis omnis, totam placeat impedit
+            distinctio vel voluptatem unde aut nisi earum. Quae blanditiis ad
+            officiis harum quisquam.
+          </p>
+          <button className="bg-blue-600 text-white px-8 py-3 rounded cursor-pointer">
+            Learn More
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
